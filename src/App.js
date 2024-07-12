dd0137763f455f269ed645f825ca206e4a7b315d
Pragya45 <pphuyal170@gmail.com>
2024-07-12 10:00:00 +0545

    Added main App component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import Navbar from "./components/Navbar";
+import ForgotPasswordPage from "./pages/auth/ForgotPasswordPage";
+import LoginPage from "./pages/auth/LoginPage";
+import RegisterPage from "./pages/auth/RegisterPage";
+import ResetPasswordPage from "./pages/auth/ResetPasswordPage";
+import Verify2FAPage from "./pages/auth/Verify2FAPage";
+import ArticlesPage from "./pages/user/ArticlesPage";
+import ChangePasswordPage from "./pages/user/ChangePasswordPage";
+import DashboardPage from "./pages/user/DashboardPage";
+import HomePage from "./pages/user/HomePage";
+import LikedAndSavedPage from "./pages/user/LikedAndSavedPage";
+import QuizDetailPage from "./pages/user/QuizDetailPage";
+import QuizPage from "./pages/user/QuizPage";
+import SubmitReport from "./pages/user/SubmitReport";
+import UserReports from "./pages/user/UserReports";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/articles" element={<ArticlesPage />} />
+        <Route path="/quizzes" element={<QuizPage />} />
+        <Route path="/quizzes/:quizId" element={<QuizDetailPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/submit-report" element={<SubmitReport />} />
+        <Route path="/my-reports" element={<UserReports />} />
+        <Route path="/liked-saved" element={<LikedAndSavedPage />} />
+        <Route path="/change-password" element={<ChangePasswordPage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/reset-password/:id/:token" element={<ResetPasswordPage />} />
+        <Route path="/verify-2fa" element={<Verify2FAPage />} />
+      </Routes>
+    </Router>
   );
 }
 
